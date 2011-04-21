9566dc17a110b53285d24e083ad431288eb2bd85
Julik <me@julik.nl>
2011-04-21 20:17:07 +0200

    Mods to Lin makefile and rapid test scripts

--- rapidTest.sh
+++ rapidTest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make -f Makefile.mac clean && make -f Makefile.mac && cp SyLens.dylib ~/.nuke/ && nukex sample.nk 
+make -f Makefile64.mac clean && make -f Makefile64.mac && cp SyLens.dylib ~/.nuke/ &&  /Applications/Nuke6.1v2/Nuke6.1v2.app/Nuke6.1v2 sample.nk 
