f9243961047204c4ed0e9e1d8d467deed573f5aa
Julik Tarkhanov <me@julik.nl>
2012-06-14 11:46:21 +0200

    Add method comments to the distorter header

--- src/SyDistorter.h
+++ src/SyDistorter.h
@@ -26,7 +26,8 @@ class SyDistorter
 {
 
 private:
-
+   
+	// The cubic parameter will usually have the opposite sign of the main distortion (ie one is positive, the other negative).
 	double k_, k_cube_, aspect_, center_shift_u_, center_shift_v_;
 	U64 hash;
 	Lut lut;
@@ -38,21 +39,44 @@ public:
 	
 	// Sets the coefficients that affect the distortion lookup table.
 	void set_coefficients(double k, double k_cube, double aspect);
+	
 	// Externally set the aspect
 	void set_aspect(double);
+	
+	// Returns the current aspect
+	double aspect();
+	
 	// Sets centerpoint shifts
 	void set_center_shift(double u, double v);
 	
+	// Removes distortion in-place from the Vector2 at the passed reference.
+	// The passed vector should be in the [-1..1, -1..1] coordinates used in Syntheyes
 	void remove_disto(Vector2&);
+	
+	// Applies distortion in-place to the Vector2 at the passed reference.
+	// The passed vector should be in the [-1..1, -1..1] coordinates used in Syntheyes
 	void apply_disto(Vector2&);
+	
+	// Applies distortion to the passed Nuke UV UVW Vector4 coordinates at the passed reference.
+	// The UV coordinates should be premultiplied by the W component and be in the [0..1, 0..1] coordinates
 	void distort_uv(Vector4& uv);
 	
+	// Generates knobs into the passed knob callback, but without the aspect control
+	// The knobs will control the variables in the object directly
 	void knobs(Knob_Callback f);
+
+	// Generates knobs into the passed knob callback, including the aspect knov
+	// The knobs will control the variables in the object directly
 	void knobs_with_aspect(Knob_Callback f);
 	
+	// Call this from _validate(). This will, if necessary, update the internal LUT
+	// used by the distortion algorithm.
 	void recompute_if_needed();
+	
+	// Returns the hash of all the distortion controls. This hash value can be used to
+	// uniquely classify the distortion model
 	U64 compute_hash();
-	double aspect();
+
 	
 private:
 	double lerp(double x, double left_x, double right_x, double left_y, double right_y);
