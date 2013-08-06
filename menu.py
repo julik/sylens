974985c6dabccab05468665e02b81e8245dc082b
Julik Tarkhanov <me@julik.nl>
2013-08-06 22:24:23 +0200

    Add binaries and Python init scripts

---
+++ menu.py
@@ -0,0 +1,8 @@
+import nuke
+
+# Inject our own node bar
+toolbar = nuke.menu("Nodes")
+sy = toolbar.addMenu( "SyLens")
+nodes = ('SyLens', 'SyCamera', 'SyUV', 'SyShader')
+for nodename in nodes:
+  sy.addCommand(nodename, lambda: nuke.createNode(nodename))
---
+++ sylens.py
@@ -0,0 +1,17 @@
+import os, sys, nuke
+
+mydir = os.path.dirname(__file__)
+nuke.pluginAppendPath(os.path.join(mydir, "gizmos"))
+
+# Add platform-dependent plugins
+platform = []
+if nuke.env['MACOS']:
+  platform.append('mac')
+if nuke.env['LINUX']:
+  platform.append('linux')
+if nuke.env['WIN32']:
+  platform.append('win')
+platform.append('-')
+platform.append(str(nuke.NUKE_VERSION_MAJOR))
+
+nuke.pluginAppendPath(os.path.join(mydir, "plugins-64", ''.join(platform)))
