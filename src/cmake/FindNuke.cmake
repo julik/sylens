057758dc7349a320436cf79f4a1b56415cb3954b
Sebastian Elsner <sebastian@risefx.com>
2014-01-12 12:46:35 +0100

    Make cmake list work improved find module

--- src/cmake/FindNuke.cmake
+++ src/cmake/FindNuke.cmake
@@ -1,20 +1,28 @@
-#TODO:
-# - Linux/Mac Support
+#TODO: Mac Support, check NUKE_ already defined via -D NUKE_...?
 
-file(GLOB NUKE_INSTALL_DIRS "C:/Program Files/Nuke*")
+if (${WIN32})
+    file(GLOB NUKE_INSTALL_DIRS "C:/Program Files/Nuke*")
+elseif (${UNIX})
+    file(GLOB NUKE_INSTALL_DIRS "/usr/local/Nuke*")
+endif()
 
-message(STATUS "NDK_PATH = " $ENV{NDK_PATH})
-list(GET NUKE_INSTALL_DIRS -1 NEWEST_NUKE_INSTALL_DIR)
-message(STATUS "NEWEST_NUKE_INSTALL_DIR = " ${NEWEST_NUKE_INSTALL_DIR})
+if(NUKE_INSTALL_DIRS)
+    list(GET NUKE_INSTALL_DIRS -1 NEWEST_NUKE_INSTALL_DIR)
+else()
+    set(NEWEST_NUKE_INSTALL_DIR "")
+endif()
 
 find_path(NUKE_INCLUDE_DIR
-		  DDImage/Op.h 
-		  PATHS $ENV{NDK_PATH} ${NEWEST_NUKE_INSTALL_DIR}
-		  PATH_SUFFIXES include)
+          DDImage/Op.h 
+          PATHS $ENV{NDK_PATH} ${NEWEST_NUKE_INSTALL_DIR}
+          PATH_SUFFIXES include)
 
 find_library(NUKE_LIBRARY
-			 DDImage
-			 PATHS $ENV{NDK_PATH} ${NEWEST_NUKE_INSTALL_DIR})
-  
+             DDImage
+             PATHS $ENV{NDK_PATH} ${NEWEST_NUKE_INSTALL_DIR})
+
+set(NUKE_LIBRARIES ${NUKE_LIBRARY} )
+set(NUKE_INCLUDE_DIRS ${NUKE_INCLUDE_DIR} )
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Nuke DEFAULT_MSG NUKE_LIBRARY NUKE_INCLUDE_DIR)
